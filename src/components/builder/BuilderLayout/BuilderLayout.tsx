c253a35f740dab3f13f6d7dd8aa41eb8555d60d5
awagl <anujwagle@gmail.com>
2024-11-04 01:27:18 -0500

    Added framer-motion library

--- src/components/builder/BuilderLayout/BuilderLayout.tsx
+++ src/components/builder/BuilderLayout/BuilderLayout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { FC, ReactNode } from 'react';
-import { motion } from 'framer-motion';
 import { Save, Download, Upload, Undo } from 'lucide-react';
 
 interface BuilderLayoutProps {
