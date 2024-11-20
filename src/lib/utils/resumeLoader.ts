7837a3008b2e5ffdad5423fc01a7ec42cd94d48d
awagl <anujwagle@gmail.com>
2024-11-20 17:22:10 -0500

    Use vercel blob in production; use local resume file in .data folder in development mode

--- src/lib/utils/resumeLoader.ts
+++ src/lib/utils/resumeLoader.ts
@@ -3,8 +3,13 @@ import path from 'path';
 import { Resume } from '@/types/resume';
 
 const DATA_DIR = path.join(process.cwd(), '.data');
+const basicValidation = (resume: Resume) => {
+  if (!resume.basics || !resume.basics.name) {
+    throw new Error('Invalid resume format: missing required fields');
+  }
+};
 
-export async function loadResume(): Promise<Resume> {
+export async function loadLocalResume(): Promise<Resume> {
   try {
     // Ensure .data directory exists
     if (!fs.existsSync(DATA_DIR)) {
@@ -17,9 +22,7 @@ export async function loadResume(): Promise<Resume> {
     const resume: Resume = JSON.parse(resumeData);
 
     // Basic validation
-    if (!resume.basics || !resume.basics.name) {
-      throw new Error('Invalid resume format: missing required fields');
-    }
+    basicValidation(resume);
 
     return resume;
   } catch (error) {
@@ -27,3 +30,22 @@ export async function loadResume(): Promise<Resume> {
     throw error;
   }
 } 
+
+export async function loadRemoteResume(): Promise<Resume> {
+  //fetch the latest resume from vercel blob
+  const url = process.env.VERCEL_RESUME_BLOB_URL;
+  if(!url) {
+    throw new Error('Env variable VERCEL_RESUME_BLOB_URL is not set');
+  }
+  const resume = await fetch(url);
+  const resumeData = await resume.json();
+  basicValidation(resumeData);
+  return resumeData;
+}
+
+export async function loadResume(): Promise<Resume> {
+  if(process.env.NODE_ENV === 'development') {
+    return loadLocalResume();
+  }
+  return loadRemoteResume();
+}
